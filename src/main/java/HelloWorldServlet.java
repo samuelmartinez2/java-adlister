b88fd4c1d0e11f6c41dfe5af94576333bb3bd1b1
sam's macbook <samuel2.martinez2@gmail.com>
2022-07-15 10:47:56 -0500

    lesson complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,23 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.rmi.ServerException;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException , ServletException {
+        String name= request.getParameter("name");
+        if (name == null) {
+            name = "world";
+        }else if (name.equalsIgnoreCase("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        }
+        request.setAttribute("name",name);
+        request.getRequestDispatcher("/WEB-INF/hello-world.jsp").forward(request,response);
+//        response.getWriter().println("<h1>Hello, World!</h1>");
     }
 }
---
+++ src/main/webapp/WEB-INF/hello-world.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: samuelmartinez
+  Date: 7/15/22
+  Time: 10:06 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello world</title>
+</head>
+<body>
+<h1>Hello, ${name}!</h1>
+
+</body>
+</html>

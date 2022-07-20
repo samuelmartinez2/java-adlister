c8a84f0d5979e47d6abaa923962319f473932ddd
sam's macbook <samuel2.martinez2@gmail.com>
2022-07-20 10:00:01 -0500

    adding config gitignore

---
+++ src/main/java/AlbumsDao.java
@@ -0,0 +1,58 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumsDao {
+    private Connection connection = null;
+
+    public AlbumsDao() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false",
+                    "root",
+                    "codeup"
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to database", e);
+        }
+    }
+
+    public List<Album> all() {
+        try {
+            Statement stmt = connection.createStatement();
+            // ResultSet needs to be handled so we create an instance.
+            ResultSet rs =stmt.executeQuery("SELECT * FROM albums");
+            List <Album> albums = new ArrayList<>();
+            while (rs.next()) {
+                albums.add(new Album(
+                        rs.getLong("id"),
+                        rs.getString("artist"),
+                        rs.getString("name"),
+                        rs.getInt("release_date"),
+                        rs.getDouble("sales"),
+                        rs.getString("genre")
+                ));
+            }
+            return albums;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error Connection to database", e);
+        }
+
+            }
+
+    public static void main(String[] args) {
+        AlbumsDao albumsDao = new AlbumsDao();
+        System.out.println();
+        List<Album>albums= albumsDao.all();
+        for (Album album : albums) {
+            System.out.println(album.getName());
+
+        }
+    }
+
+
+}

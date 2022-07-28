9a674547689fa3d85268784f56def3397753394b
sam's macbook <samuel2.martinez2@gmail.com>
2022-07-28 15:57:39 -0500

    java-adlister-update

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -27,6 +27,11 @@ public class ListAdsDao implements Ads {
         return ad.getId();
     }
 
+    public String update(Ad ad){
+        ad.getTitle(); ad.getDescription();
+        return ad.setTitle(); ad.setDescription(); ;
+    }
+
     private List<Ad> generateAds() {
         List<Ad> ads = new ArrayList<>();
         ads.add(new Ad(
